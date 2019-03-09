062e1d8543c80ea303bff090e3c04d0c45992140
Ramses Flouw <ramses.flouw@hotmail.com>
2019-03-09 10:43:06 +0100

    Add first webserver

---
+++ server.js
@@ -0,0 +1,6 @@
+const webServer = require('./custom_modules/webserver.js');
+
+console.log('******* STARTING APPLICATION. *******');
+
+console.log('STARTING WEBSERVER...');
+webServer.startWebServer();
